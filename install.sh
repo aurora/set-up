821ab12be103b520711bc8b3cb50437e7bf6d2a4
Harald Lapp <harald.lapp@gmail.com>
2012-10-09 12:42:06 +0200

    added compiler step for erlang and install erlang related stuff only if erlang compiler is in path

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 SRC=$HOME/dotfiles
 
+# install erlang related stuff
+ERLC=$(which erlc)
+
+if [ "$ERLC" != "" ]; then
+    $ERLC -o $SRC/erl/ $SRC/erl/user_default.erl 
+fi
+
 ln -snf $SRC/erlang $HOME/.erlang
 
+# install stuff from etc folder
 ln -snf $SRC/etc/vimrc $HOME/.vimrc
 ln -snf $SRC/etc/vim $HOME/.vim
