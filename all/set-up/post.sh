605dd44a405bf42e6221812b306393ef6c04808d
Harald Lapp <h.lapp@clipdealer.de>
2013-10-30 13:35:22 +0100

    initial commit post-install script

---
+++ all/set-up/post.sh
@@ -0,0 +1,5 @@
+# install erlang related stuff only if erlang is available on the system
+if command -v erlc >/dev/null 2>&1; then
+    erlc -o $SRC/all/set-up/erl/ $SRC/all/set-up/erl/aur.erl 
+    ln -snf $SRC/all/set-up/.erlang $HOME/.erlang
+fi
