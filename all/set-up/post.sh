9cf72002a480afcdcbe34dcdcee90f98ef0a671f
Harald Lapp <h.lapp@clipdealer.de>
2015-09-09 10:13:57 +0200

    removed php switcher

--- all/set-up/post.sh
+++ all/set-up/post.sh
@@ -5,11 +5,3 @@ if command -v erlc >/dev/null 2>&1; then
     erlc -o $SRC/all/set-up/erl/ $SRC/all/set-up/erl/aur.erl 
     ln -snf $SRC/all/set-up/.erlang $HOME/.erlang
 fi
-
-# install php switcher only if php is present
-PHP_DIR=${PHP_DIR:-/opt}            # path to installed php versions
-PHP_CNT=$(find /opt -name "php-*" -prune \( -type d -and ! -type l \) | wc -l)
-
-if [ "$PHP_CNT" != "0" ]; then
-    ln -snf $SRC/all/set-up/bin/switch-php.sh $HOME/bin/
-fi
